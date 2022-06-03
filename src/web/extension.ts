4bd2cc121021811e5367649c26ce5ae085df2b92
alingse <alingse@foxmail.com>
2022-06-03 22:16:54 +0800

    rename command

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -13,11 +13,11 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('thirft-formatter.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('thirft-formatter.formatThriftFile', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from thirft formatter in a web extension host!');
+		vscode.window.showInformationMessage('Your Thrift file has been formatted');
 	});
 
 	context.subscriptions.push(disposable);
