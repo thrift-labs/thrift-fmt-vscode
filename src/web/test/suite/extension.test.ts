9f6770456ba0fbb8fba9de703d3601e05ab7dea1
alingse <alingse@foxmail.com>
2022-07-30 00:15:27 +0800

    upgrade version and add more test

--- src/web/test/suite/extension.test.ts
+++ src/web/test/suite/extension.test.ts
@@ -10,4 +10,12 @@ suite('Web Extension Test Suite', () => {
 		assert.ok(ok);
 		assert.equal(content, 'include "shared.thrift"');
 	});
+
+	test('Sample test', () => {
+		const rawContent = `const  string default_user = "\\'default_user\\'" ;
+        const string default_name = '"abc\\'s"';`
+        const [content, ok] = fmtExt.formatThrift(rawContent, true, 4);
+		assert.ok(ok);
+		assert.equal(content.length, 89);
+	});
 });
